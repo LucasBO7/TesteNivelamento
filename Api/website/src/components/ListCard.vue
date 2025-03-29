8a8356e632755ccd8a13518a932364042bd34c40
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2025-03-28 22:11:20 -0300

    Realizado componentização, desenvolvido design da tela inicial e integração com o servidor em andamento

---
+++ Api/website/src/components/ListCard.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="line-card-container" v-for="(item, index) in cards" :key="index">
+        <ul>
+            <li>{{ item.nomeFantasia }}</li>
+            <li>{{ item.razaoSocial }}</li>
+            <li>{{ item.cnpj }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Card {
+    nomeFantasia: string;
+    razaoSocial: string;
+    cnpj: string;
+}
+
+defineProps<{
+    cards: Card[];
+}>();
+</script>
+
+<style scoped>
+.line-card-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 55px;
+    margin-bottom: 15px;
+    box-shadow: 0px 5px 10px 0px black;
+    border-radius: 15px;
+    border-left: 3px solid rgb(2, 100, 156);
+    background-color: transparent;
+}
+
+ul {
+    display: flex;
+    justify-content: space-between;
+    text-decoration: none;
+    width: 100%;
+    height: 100%;
+    list-style: none;
+    border-radius: 15px;
+}
+
+ul:hover {
+    background-color: rgb(26, 26, 26);
+    color: rgb(2, 100, 156);
+    cursor: pointer;
+    transition: 0.2s ease-out;
+}
+
+li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 33%;
+    text-align: center;
+}
+</style>
---
+++ Api/website/src/components/ListSection.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="list-section">
+        <ListLine :cards="cardData" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import ListLine from './ListCard.vue';
+import { onMounted, ref } from 'vue';
+
+interface Card {
+    nomeFantasia: string;
+    razaoSocial: string;
+    cnpj: string;
+}
+
+const cardData = ref<Card[]>([]);
+const searchText = ref('');
+
+// const cardData = ref<Card[]>([
+//     { nomeFantasia: 'Card 1', razaoSocial: 'Descrição do Card 1', cnpj: '12345678901234' },
+//     { nomeFantasia: 'Card 2', razaoSocial: 'Descrição do Card 2', cnpj: '12345678901234' },
+//     { nomeFantasia: 'Card 3', razaoSocial: 'Descrição do Card 3', cnpj: '12345678901234' }
+// ]);
+
+const fetchData = async () => {
+    const formData = new FormData();
+
+    // formData.append("")
+
+    await axios.postForm("",)
+        .then(response => console.log("RESPOSTA => ", response))
+        .catch(error => console.error('Houve um erro ao carregar os dados: ', error))
+}
+
+onMounted(fetchData);
+</script>
+
+<style scoped>
+.list-section {
+    height: 55vh;
+    overflow-y: auto;
+}
+</style>
